7096c36cc3d11612677b456dc5b4569ffeeeef4a
unknown <dd38@students.calvin.edu>
2019-09-27 14:57:05 -0400

    a react native calculator app

--- lab03/lab03-complete/App.tsx
+++ lab03/lab03-complete/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Image } from 'react-native'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, Shell Squad!</Text>
-    </View>
-  );
+
+// export default class testText extends Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+//           <Text>Hello, world!</Text>
+//       </View>
+//       ); 
+//     }
+//   }
+
+export default class testPicture extends Component {
+  render() {
+    let pic = {
+      uri: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+      
+    );
+  }
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
